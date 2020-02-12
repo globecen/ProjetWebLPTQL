83cf000028fa47e182af1e3d7774605616555793
globecen <34088599+globecen@users.noreply.github.com>
2020-02-12 12:31:27 +0100

    geg

--- connexion.php
+++ connexion.php
@@ -1,19 +1,18 @@
 <?php
 include_once('db.inc.php');
-//session_start();
+session_start();
 $emailform = $_POST['email'];
 $passform = $_POST['pass']; 
-//$_SESSION['email'] = 'email';
+$_SESSION['login'] = 'email';
 //$_SESSION['statut'] = 'statut';
 $db=new PDO("$server:host=$host;port=$port;dbname=$base", $user, $pass);
-$sql="SELECT * from _Utilisateurs where login=$emailform and mdp=$passform";
-if ($db->query($sql)){
-echo "ok";
-}	
-echo "ko";
+$sql="SELECT * from _Utilisateurs where login='$emailform' and mdp='$passform'";
+foreach ($db->query($sql) as $ligne){
+	echo 'Vous etes connecter '.$ligne['login'];
+}
 ?>
 <center>
-<form method="post" action="#">
+<form method="post" action="">
 	Email : <input type="text" name="email"> <br>
 	Mot de passe : <input type="password" name="pass"><br>
 	<input type="submit" value="Se connecter">
