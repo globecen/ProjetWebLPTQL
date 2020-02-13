f590f01b5b2bd18982a3f17bd520f1f0ecc5d18b
jules-goose <i192678@IUTLAVALAD.UNIV-LEMANS.FR>
2020-02-13 13:28:27 +0100

    Signed-off-by: jules-goose <i192678@IUTLAVALAD.UNIV-LEMANS.FR>

--- addJsCustom.php
+++ addJsCustom.php
@@ -1,6 +1,8 @@
 <?php
 ?>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous">
+</script>
+<script>
     function getIdActivite(value){
         var valIdActivite = value;
         console.log(valIdActivite);
