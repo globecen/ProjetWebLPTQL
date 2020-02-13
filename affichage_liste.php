29b40c95b87c8b45c38c5a5bea3fa242b6c018ce
jules-goose <i192678@IUTLAVALAD.UNIV-LEMANS.FR>
2020-02-13 13:17:55 +0100

    Signed-off-by: jules-goose <i192678@IUTLAVALAD.UNIV-LEMANS.FR>

--- affichage_liste.php
+++ affichage_liste.php
@@ -6,7 +6,6 @@
         display: grid;
         grid-template-columns: 90% 10% ;
         grid-template-rows: 10% auto;
-        background: aqua;
         justify-content: stretch;
 
     }
@@ -15,14 +14,14 @@
         grid-column-end: 2;
         grid-row-start:1 ;
         grid-row-end: 2;
-        background: chocolate;
+
     }
     .child-grid-new-fiche{
         grid-column-start: 2;
         grid-column-end: 3;
         grid-row-start:1 ;
         grid-row-end: 2;
-        background-color: indigo;
+        background-color: grey;
         color: white;
         display: block;
         text-align: center;
@@ -33,7 +32,7 @@
         grid-column-end: 3;
         grid-row-start: 2;
         grid-row-end: 3;
-        background: grey;
+
 
     }
     .container-liste-flex{
@@ -44,7 +43,7 @@
         flex-flow: row wrap;
         justify-content: left;
         align-items: flex-start;
-        background: coral;
+
     }
     .child-liste-flex{
         flex: 0 1 20%;
@@ -58,37 +57,18 @@
 //include("header.php")
 ?>
 
-<div class="container-content-grid">
-
 
     <?php
-    echo ("<div class=\"child-grid-liste-name\">");
-    echo ("<h1>test</h1>");
-    echo ("</div>");
-    echo ("<a class=\"child-grid-new-fiche\" href=\"add_fiche_liste.php\">nouvelle fiche</a>");
-    echo ("<div class=\"child-grid-liste-flex\">");
-    echo ("<div class=\"container-liste-flex\">");
-    
-   /* include_once('db.inc.php');
+    include_once('db.inc.php');
     $db=new PDO("$server:host=$host;port=$port;dbname=$base", $user, $pass);
-    $idActivity =
-    $sql="SELECT description_f FROM Fiches WHERE id_f = id_f and id_a = $idActivity ";
+    $idActivity = 1;
+    $sql="SELECT Fiches.description_f FROM Fiches,Associer WHERE Fiches.id_f = Associer.id_f AND Associer.id_a =1 ";
     foreach ($db->query($sql) as $fiche){
         echo("<div class=\"child-liste-flex\">");
         echo($fiche);
         echo("</div>");
     }
-    */
-    
-    for ($i = 0;$i <7;$i++){
-        echo("<div class=\"child-liste-flex\">");
-        echo($i);
-        echo("</div>");
-    }
-    echo ("</div>");
-    echo ("</div>");
     ?>
-</div>
 <?php
 //include ("footer.php")
 ?>
